13f7cec1e98b91d540c27ab18cca93b8b9b05a55
unknown <radyan6661@gmail.com>
2023-08-06 21:14:55 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import navbar from  './components/navbar.vue'
+</script>
+
+<template>
+  <div class="bg-cover bg-center bg-no-repeat bg-[url('./assets/icons/background.webp')] h-full w-full">
+    <div class="h-[100vh] px-6">
+      <navbar/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,34 @@
+<template lang="">
+    <div class="container m-auto flex justify-between h-28 items-center text-white">
+        <div class="">
+            <img src="../assets/icons/Lush.svg" alt="" class="w-28">
+        </div>
+        <div class="text-lg flex space-x-14">
+            <ul class="flex space-x-14">
+                <li v-for="link in links" :key="link">
+                    <a :href="link.link"
+                        class="relative after:contents-[''] after:h-[2px] after:w-0 after:left-0 after:bg-accent after:absolute after:bottom-[-6px] after:duration-300 hover:after:w-3 cursor-pointer"> 
+                        {{link.name}}
+                    </a>
+                </li>
+            </ul>
+            <button class="outline outline-1 px-8 py-1 rounded-[3px]">Call Us</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    setup() {
+        let links = [
+            { name: "Home", link: "#" },
+            { name: "About Us", link: "#" },
+            { name: "Planters", link: "#" },
+            { name: "Contact", link: "#" },
+        ]
+        return { links }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
