9dbbed00af88238d0a8e2d198f614d0778b57ec7
unknown <radyan6661@gmail.com>
2023-08-07 18:58:04 +0700

    adding home pages

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import navbar from  './components/navbar.vue'
+import home from  './components/home.vue'
 </script>
 
 <template>
   <div class="bg-cover bg-center bg-no-repeat bg-[url('./assets/icons/background.webp')] h-full w-full">
     <div class="h-[100vh] px-6">
       <navbar/>
+      <home/>
     </div>
   </div>
 </template>
---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div class="absolute h-full w-full top-0 left-0 flex justify-center items-center">
+        <div class="container m-auto text-white text-center space-y-4">
+            <h1 class="text-[64px] font-bold">Nature's Beauty Delivered to You</h1>
+            <p class="text-lg">Nature's beauty is just a click away with our online flower and plant shop. <br> We offer a wide variety of flowers that will bring a touch of nature to your home!</p>
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
