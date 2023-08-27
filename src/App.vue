560c3bb6832f96c7b6825640960ac66fe167fd97
Radyan Bintang <113041507+rdynxyz@users.noreply.github.com>
2023-08-27 21:40:09 +0700

    change source background

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import thirdPage from  './components/third-page.vue'
 </script>
 
 <template>
-  <div class="bg-cover bg-center bg-no-repeat bg-[url('/assets/icons/background.webp')] h-[100vh] w-full">
+  <div class="bg-cover bg-center bg-no-repeat bg-[url('/assets/images`/background.webp')] h-[100vh] w-full">
     <div class="h-[100vh] px-6">
       <navbar/>
       <home/>
