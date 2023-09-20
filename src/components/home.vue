b9ecfd09fa4e48ec35b098a1a04c4c9a2711858d
Radyan Bintang <113041507+rdynxyz@users.noreply.github.com>
2023-09-20 20:22:09 +0700

    revisi responsive

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,9 +3,9 @@
         <div class="container m-auto text-white text-center">
             <h1 class="text-[40px] sm:text-[64px] font-bold font-Lato">Nature's Beauty Delivered to You</h1>
             <p class="text-lg font-Poppins font-medium mt-4">Nature's beauty is just a click away with our online flower and plant shop. We offer a <br> wide variety of flowers that will bring a touch of nature to your home!</p>
-            <div class="flex justify-center gap-[13PX] mt-9">
-                <button class="px-[50px] py-3 rounded-[3px] bg-[#285A43] font-bold font-Lato">Book Now</button> 
-                <button class="px-9 py-3 rounded-[3px] outline outline-1 font-bold font-Lato flex gap-[10px]">
+            <div class="flex flex-col sm:flex-row justify-center items-center gap-[13PX] mt-9">
+                <button class="px-[50px] py-3 rounded-[3px] bg-[#285A43] font-bold font-Lato w-[175px]">Book Now</button> 
+                <button class="px-9 py-3 rounded-[3px] outline outline-1 font-bold font-Lato flex gap-[10px] w-[199px]">
                     <img src="/assets/icons/play-button.svg" alt="" srcset=""> Watch Video</button>
             </div>
         </div>
