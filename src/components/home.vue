5b05d2bc3ac47409489c5d5ad3c489b82201b80d
Radyan Bintang <113041507+rdynxyz@users.noreply.github.com>
2023-09-24 14:09:47 +0700

    compress bg image

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,8 +1,8 @@
 <template lang="">
     <div class="absolute h-full w-full top-0 left-0 px-6 flex justify-center items-center">
-        <div class="container m-auto text-white text-center">
+        <div class="container m-auto text-white text-center flex flex-col items-center">
             <h1 class="text-[40px] sm:text-[64px] font-bold font-Lato">Nature's Beauty Delivered to You</h1>
-            <p class="text-base sm:text-lg font-Poppins font-medium mt-4">Nature's beauty is just a click away with our online flower and plant shop. We offer a <br> wide variety of flowers that will bring a touch of nature to your home!</p>
+            <p class="text-base sm:text-lg font-Poppins font-medium mt-4 lg:w-[770px]">Nature's beauty is just a click away with our online flower and plant shop. We offer a wide variety of flowers that will bring a touch of nature to your home!</p>
             <div class="flex flex-col sm:flex-row justify-center items-center gap-[13PX] mt-9">
                 <button class="px-[50px] py-3 rounded-[3px] bg-[#285A43] font-bold font-Lato w-[175px]">Book Now</button> 
                 <button class="px-9 py-3 rounded-[3px] outline outline-1 font-bold font-Lato flex gap-[10px] w-[199px]">
--- src/components/testimonials.vue
+++ src/components/testimonials.vue
@@ -1,13 +1,52 @@
 <template lang="">
-    <div>
-        <h1 class="text-center text-primary text-4xl font-Lato font-extrabold">What do they say about us</h1>
+  <div>
+    <h1 class="text-center text-primary text-4xl font-Lato font-extrabold">
+      What do they say about us
+    </h1>
+    <div class="mt-9 place-items-center grid grid-cols-3">
+      <div
+        class="bg-[#f8f8f8] w-[360px] h-80 rounded-[10px] p-12"
+        v-for="testimonial in testimonials"
+        :key="testimonial"
+      >
+        <div class="flex items-center gap-5">
+          <img
+            :src="`/assets/icons/testimonials/${testimonial.icon}`"
+            alt=""
+            srcset=""
+          />
+          <h1 class="font-bold text-primary text-xl">{{ testimonial.name }}</h1>
+        </div>
+        <p class="mt-8">“ {{ testimonial.content }} ”</p>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  setup() {
+    let testimonials = [
+      {
+        icon: "testi1.svg",
+        name: "Jessica Watson",
+        content:
+          "Highly recommend this website for quality flowers and plants. Great prices, timely delivery and excellent customer service.",
+      },
+      {
+        icon: "testi2.svg",
+        name: "Kate Szu ",
+        content:
+          "Great service, beautiful flowers, timely delivery. Highly recommend.",
+      },
+      {
+        icon: "testi3.svg",
+        name: "Grace ",
+        content:
+          "I am very happy with my purchase from this website, the plants were healthy and arrived on time.",
+      },
+    ];
+    return { testimonials };
+  },
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
