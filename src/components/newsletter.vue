f01126b0f953f46944759e2f46f3c7a36400ff65
Radyan Bintang <113041507+rdynxyz@users.noreply.github.com>
2023-09-28 16:33:31 +0700

    newsletter

--- src/components/newsletter.vue
+++ src/components/newsletter.vue
@@ -4,7 +4,8 @@
             <h1 class="font-Lato font-bold text-[32px] text-white z-50 text-start ">Enter your email address for our mailing Promo or other interesting things</h1>
         </div>
         <div class="xl:w-1/2 pl-6">
-            <input type="text" name="email" id="email" placeholder="Enter your email" class="bg-blur/50 bg-transparent text-white w-[475px] h-12 rounded-md pl-5 outline outline-1 outline-white border-white focus:outline-1">
+            <input type="text" name="email" id="email" placeholder="Enter your email" 
+            class="bg-blur/50 bg-transparent text-white w-[475px] h-12 rounded-md pl-5 outline outline-1 outline-white border-white focus:outline-1">
         </div>
     </div>
 </template>
--- src/components/testimonials.vue
+++ src/components/testimonials.vue
@@ -3,21 +3,21 @@
     <h1 class="text-center text-primary text-4xl font-Lato font-extrabold">
       What do they say about us
     </h1>
-    <div class="mt-9 place-items-center grid grid-cols-3">
+    <div class="mt-9 place-items-center grid grid-cols-3 gap-4 xl:gap-0">
       <div
-        class="bg-[#f8f8f8] w-[360px] h-80 rounded-[10px] p-12"
+        class="bg-[#f8f8f8] xl:w-[360px] h-full xl:h-80 rounded-[10px] p-6 lg:p-12"
         v-for="testimonial in testimonials"
         :key="testimonial"
       >
-        <div class="flex items-center gap-5">
+        <div class="flex flex-col lg:flex-row items-center item-center lg:justify-start gap-2 lg:gap-5">
           <img
             :src="`/assets/icons/testimonials/${testimonial.icon}`"
             alt=""
             srcset=""
           />
-          <h1 class="font-bold text-primary text-xl">{{ testimonial.name }}</h1>
+          <h1 class="font-bold text-primary text-xl text-center lg:text-start">{{ testimonial.name }}</h1>
         </div>
-        <p class="mt-8">“ {{ testimonial.content }} ”</p>
+        <p class="lg:mt-8 mt-2">“ {{ testimonial.content }} ”</p>
       </div>
     </div>
   </div>
@@ -28,19 +28,19 @@ export default {
     let testimonials = [
       {
         icon: "testi1.svg",
-        name: "Jessica Watson",
+        name: "J. Watson",
         content:
           "Highly recommend this website for quality flowers and plants. Great prices, timely delivery and excellent customer service.",
       },
       {
         icon: "testi2.svg",
-        name: "Kate Szu ",
+        name: "Kate Szu",
         content:
           "Great service, beautiful flowers, timely delivery. Highly recommend.",
       },
       {
         icon: "testi3.svg",
-        name: "Grace ",
+        name: "Grace",
         content:
           "I am very happy with my purchase from this website, the plants were healthy and arrived on time.",
       },
