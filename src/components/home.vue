b771b8f22022a9c7793cb4bd77676834c0aee411
Radyan Bintang <113041507+rdynxyz@users.noreply.github.com>
2025-04-13 18:58:26 +0700

    test 3

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <div class="absolute h-full w-full top-0 left-0 px-6 flex justify-center items-center">
         <div class="container m-auto text-white text-center flex flex-col items-center">
-            <h1 class="text-[40px] sm:text-[64px] font-bold font-Lato">Nature's Beauty Delivered to You</h1>
+            <h1 class="text-[40px] sm:text-[64px] font-bold font-Lato">Nature's Beauty Delivered to You test 3</h1>
             <p class="text-base sm:text-lg font-Poppins font-medium mt-4 lg:w-[770px]">Nature's beauty is just a click away with our online flower and plant shop. We offer a wide variety of flowers that will bring a touch of nature to your home!</p>
             <div class="flex flex-col sm:flex-row justify-center items-center gap-[13PX] mt-9">
                 <button class="px-[50px] py-3 rounded-[3px] bg-[#285A43] font-bold font-Lato w-[175px]">Book Now</button> 
